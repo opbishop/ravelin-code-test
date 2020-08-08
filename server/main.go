208f41a5817ad04105aba3fd41a5d559d1df6f64
Oli <oliverbishop90@gmail.com>
2020-08-08 12:08:11 +0100

    build struct from json, update Data & store in db

--- server/main.go
+++ server/main.go
@@ -1,23 +1,108 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func handleNewData(w http.ResponseWriter, r *http.Request) {
+type Data struct {
+	WebsiteUrl         string
+	SessionId          string
+	ResizeFrom         Dimension
+	ResizeTo           Dimension
+	CopyAndPaste       map[string]bool // map[fieldId]true
+	FormCompletionTime int             // Seconds
+}
+
+type Dimension struct {
+	Width  string
+	Height string
+}
+
+type TrackingEvent struct {
+	WebsiteUrl string `json:"websiteUrl"`
+	SessionId  string `json:"sessionId"`
+}
+
+type ResizeEvent struct {
+	*TrackingEvent
+	OldWidth  string `json:"oldWidth"`
+	OldHeight string `json:"oldHeight"`
+	NewWidth  string `json:"newWidth"`
+	NewHeight string `json:"newHeight"`
+}
+
+func getRecord(sessionId string, websiteUrl string) Data {
+	if record, exist := db[sessionId]; exist {
+		return record
+	} else {
+		return Data{WebsiteUrl: websiteUrl, SessionId: sessionId, CopyAndPaste: make(map[string]bool)}
+	}
+}
+
+func createDim(height string, width string) Dimension {
+	return Dimension{
+		Width:  width,
+		Height: height,
+	}
+}
+
+func handleResizeEvent(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	fmt.Println("New POST received")
+	fmt.Println("New resize event")
 	fmt.Println(string(reqBody))
+
+	var event ResizeEvent
+	json.Unmarshal(reqBody, &event)
+
+	record := db[event.SessionId]
+	record.ResizeFrom = createDim(event.OldHeight, event.OldWidth)
+	record.ResizeTo = createDim(event.NewHeight, event.NewWidth)
+	record.SessionId = event.SessionId
+	record.WebsiteUrl = event.WebsiteUrl
+
+	db[event.SessionId] = record
+
+	fmt.Println(db[event.SessionId])
+
+}
+
+type CopyPasteEvent struct {
+	*TrackingEvent
+	EventType string `json:"eventType"`
+	Pasted    bool   `json:"pasted"`
+	FormId    string `json:"inputCardNumber"`
+}
+
+func handleCopyPasteEvent(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	fmt.Println("New copy-paste event")
+	fmt.Println(string(reqBody))
+
+	var event CopyPasteEvent
+	json.Unmarshal(reqBody, &event)
+
+	record := getRecord(event.SessionId, event.WebsiteUrl)
+	record.CopyAndPaste[event.FormId] = event.Pasted
+
+	db[event.SessionId] = record
+
 }
 
 func handleRequests() {
-	http.HandleFunc("/", handleNewData)
+	http.HandleFunc("/resize", handleResizeEvent)
+	http.HandleFunc("/copypaste", handleCopyPasteEvent)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
+// data structure to hold Data structs as they are completed
+// maps SessionId (string): Data struct object
+var db map[string]Data
+
 func main() {
+	db = make(map[string]Data)
 	handleRequests()
 }
